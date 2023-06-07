81e20da891d0d604f0bf5de4af4e847111c196bb
Ernesto Reis <ernestosrf98@gmail.com>
2023-06-07 17:04:40 -0300

    finished the complete system

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    boku_no_hero: ["Midoriya", "Bakugo", "Todoroki", "All Might", "Shigaraki"],
+    shingeki_no_kyojin: ["Eren", "Levi", "Mikasa", "Armin", "Reiner"],
+    kimetsu_no_yaiba: ["Tanjiro", "Nezuko", "Zenitsu", "Inosuke", "Muzan"],
+    game_of_thrones: ["Daenerys", "Sansa", "Arya", "Cersei", "Tyrion", "Jaime", "Joffrey", "Tyrwin", "Bran", "Ygritte"],
+    supernatural: ["Dean", "Sam", "Castiel", "Crowley", "Lucifer", "John"],
+    the_middle: ["Brick", "Sue", "Frankie", "Axl", "Mike", "Bob", "Carly", "Brad"]
+};
